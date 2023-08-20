a34ede39c9f58dcf2dbc00ceabd7d550533f4030
TJGameDev <98005357+TJGameDev@users.noreply.github.com>
2023-08-19 22:07:17 -0400

    changes to allow product unique searched for with just uuid

--- src/api/product/dto/product-where-unique.input.ts
+++ src/api/product/dto/product-where-unique.input.ts
@@ -3,7 +3,7 @@ import { Category } from 'src/api/category/model/category.model';
 
 @InputType()
 export class ProductWhereUniqueInput {
-  @Field(() => Number)
+  @Field(() => Number, { nullable: true })
   id: number;
   @Field(() => String, { nullable: true })
   uuid: string;
